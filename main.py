6e9727afbe8e919d682e6544cb5000d5e25bab89
Q R <128982989+YANS311@users.noreply.github.com>
2023-09-07 23:33:40 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+import  student
+import  student_system as sms
+
+if __name__ == '__main__':
+   stu_sms=sms.StudentManagerSystem()
+   stu_sms.start()
